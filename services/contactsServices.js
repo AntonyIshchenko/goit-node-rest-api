37d847e6347e52c9c38c5f73d2d3b1dd2c701045
Antony Ishchenko <antony.ishchenko@gmail.com>
2024-05-02 00:15:10 +0300

    done

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,71 +1,39 @@
-import * as fs from 'node:fs/promises';
-import path from 'node:path';
-import { nanoid } from 'nanoid';
-
-const contactPath = path.resolve('db', 'contacts.json');
+import Contact from '../schemas/contactsModel.js';
 
 async function listContacts() {
-    const contacts = await readFile();
-    return contacts;
+  const contacts = await Contact.find();
+  return contacts;
 }
 
 async function getContactById(contactId) {
-    const contacts = await readFile();
-    const contact = contacts.find((item) => item.id === contactId);
+  const contact = await Contact.findById(contactId);
 
-    return contact ? contact : null;
+  return contact ? contact : null;
 }
 
 async function removeContact(contactId) {
-    const contacts = await readFile();
-    const index = contacts.findIndex((item) => item.id === contactId);
-
-    if (index === -1) return null;
-
-    const contact = contacts.splice(index, 1)[0];
-
-    await writeFile(contacts);
-
-    return contact;
+  const contact = await Contact.findByIdAndDelete({ _id: contactId });
+  return contact;
 }
 
 async function addContact(name, email, phone) {
-    const contacts = await readFile();
-    const newContact = { id: nanoid(), name, email, phone };
+  const newContact = await Contact.create({ name, email, phone });
 
-    contacts.push(newContact);
-    await writeFile(contacts);
-
-    return newContact;
+  return newContact;
 }
 
 async function updateContact(id, updatedData) {
-    const contacts = await readFile();
-    const index = contacts.findIndex((item) => item.id === id);
+  const updContact = await Contact.findByIdAndUpdate({ _id: id }, updatedData, {
+    new: true,
+  });
 
-    if (index === -1) return null;
-
-    const updContact = { ...contacts[index], ...updatedData };
-    contacts[index] = updContact;
-
-    await writeFile(contacts);
-
-    return updContact;
-}
-
-async function readFile() {
-    const data = await fs.readFile(contactPath, { encoding: 'utf-8' });
-    return JSON.parse(data);
-}
-
-async function writeFile(contacts) {
-    return fs.writeFile(contactPath, JSON.stringify(contacts, null, 2));
+  return updContact;
 }
 
 export default {
-    listContacts,
-    getContactById,
-    removeContact,
-    addContact,
-    updateContact,
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
 };
